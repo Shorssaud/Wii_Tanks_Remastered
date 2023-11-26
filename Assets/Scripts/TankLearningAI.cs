d78fc5c6ab445921d6f644317f3c5f08a64a92ef
Pol Siles <pol.siles-polo@epitech.eu>
2023-11-26 17:22:29 +0900

    [ADD] AI for the brown tank

--- Assets/Scripts/TankLearningAI.cs
+++ Assets/Scripts/TankLearningAI.cs
@@ -2,20 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
-public class TankLearningAI : MonoBehaviour
+
+public class TankLearningAI : Agent
 {
-    public NavMeshAgent agent;
+    public Transform target;
+
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-            
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.destination = target.position;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // Move the target to a new spot
+        this.transform.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+        target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
         
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        actionsOut.ContinuousActions.Array[0] = Input.GetAxis("Horizontal");
+        actionsOut.ContinuousActions.Array[1] = Input.GetAxis("Vertical");
+
+        // add the movement of the tank by the AI through the use of the function Move()
+
     }
 }
