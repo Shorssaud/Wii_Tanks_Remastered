60c9336612b7a85f89610a65cc74d3161660d976
Vivant-Garrigues <72013869+Vivant-Garrigues@users.noreply.github.com>
2023-12-09 21:38:33 +0900

    [ADD] transitions

---
+++ Assets/Scripts/UI/DisplayTransition.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class DisplayTransition : MonoBehaviour
+{
+    public GameObject levelObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI levelText = levelObject.GetComponent<TextMeshProUGUI>();
+        if (levelText != null)
+        {
+            levelText.text = "Mission " + PlayerPrefs.GetInt("Level").ToString();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            string newLevel = "Scenes/Levels/Level" + PlayerPrefs.GetInt("Level").ToString();
+            UnityEngine.SceneManagement.SceneManager.LoadScene(newLevel);
+        }
+    }
+}
