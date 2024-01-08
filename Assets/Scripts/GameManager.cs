fed8f60f097c858c59a5389132ee1dbdeb863c9e
Shaun Orssaud <shaun.orssaud@epitech.eu>
2024-01-08 14:07:03 +0900

    FIX general

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Threading;
+using System.Diagnostics;
 
 public class GameManager : MonoBehaviour
 {
     private GameObject loseMessage;
     private GameObject winMessage;
     private bool onTransition = false;
+    Stopwatch GameTime;
+    Stopwatch LevelTime;
 
-    void Start()
+    public GameManager instance;
+
+    void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Don't destroy this object when loading new scenes
+        }
         loseMessage = GameObject.Find("Destroyed");
         winMessage = GameObject.Find("MissionCleared");
         loseMessage.SetActive(false);
         winMessage.SetActive(false);
+
+        GameTime = new Stopwatch();
+        LevelTime = new Stopwatch();
+        GameTime.Start();
+        LevelTime.Start();
     }
 
     void Update()
@@ -39,9 +55,9 @@ public class GameManager : MonoBehaviour
         if (GameObject.FindGameObjectsWithTag("AI").Length == 0 && !onTransition) {
             onTransition = true;
             int newLevel = PlayerPrefs.GetInt("Level") + 1;
-            Debug.Log(newLevel);
+            UnityEngine.Debug.Log(newLevel);
             string newLevelPath = "Scenes/Levels/Level" + newLevel;
-            Debug.Log(newLevelPath);
+            UnityEngine.Debug.Log(newLevelPath);
             if (!IsSceneInBuildSettings(newLevelPath)) StartCoroutine(WinAndEnd());
             else {
                 PlayerPrefs.SetInt("Level", newLevel);
@@ -67,6 +83,7 @@ public class GameManager : MonoBehaviour
         loseMessage.SetActive(true);
         yield return new WaitForSeconds(2f); // Attendre 1 seconde
         PlayerPrefs.SetInt("Lives", PlayerPrefs.GetInt("Lives") - 1);
+        UnityEngine.Debug.Log("Player lost at level" + PlayerPrefs.GetInt("Level"));
         UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Menus/LoseMenu");
     }
 
@@ -75,6 +92,7 @@ public class GameManager : MonoBehaviour
         winMessage.SetActive(true);
         //winMessage.gameObject.SetActive(true);
         yield return new WaitForSeconds(2f); // Attendre 1 seconde
+        UnityEngine.Debug.Log("Finished Level in " + LevelTime.Elapsed);
         UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Menus/Transition");
     }
 
@@ -97,5 +115,11 @@ public class GameManager : MonoBehaviour
             }
         }
         return false;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(gameObject);
+        UnityEngine.Debug.Log("Total Game Time: " +  GameTime.Elapsed);
     }
 }
